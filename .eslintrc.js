39fe4cd9426e1ee69aed7a10a50c11117e6ca111
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2021-10-20 20:01:26 +0900

    fix: no airbnb lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'airbnb-typescript/base',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
