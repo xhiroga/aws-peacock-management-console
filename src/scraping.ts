989f5cb8337ac91c76befa5add9d0c20551d2fe5
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2021-10-22 08:16:47 +0900

    feat: detect aws accounts expanded

---
+++ src/scraping.ts
@@ -0,0 +1,40 @@
+const getPortalApplicationList = () =>
+  document.getElementsByTagName('portal-application-list')[0]
+
+const isAwsAccountSelected = (): boolean => {
+  const appAwsAccount = document.querySelectorAll(
+    'portal-application[title="AWS Account"]'
+  )[0]
+  return appAwsAccount.classList.contains('selected')
+}
+
+type OnAwsAccountSelected = (stopObserve: () => void) => void
+const onAwsAccountSelected = (callback: () => void) => {
+  if (!isAwsAccountSelected()) {
+    return
+  }
+  callback()
+}
+
+const observePortalApplicationList = (
+  onAwsAccountSelected: OnAwsAccountSelected
+) => {
+  const portalApplicationList = getPortalApplicationList()
+  const mutationCallback = (
+    mutationList: MutationRecord[],
+    observer: MutationObserver
+  ) => {
+    console.log(mutationList)
+    onAwsAccountSelected(() => observer.disconnect())
+  }
+  const config = { attributes: false, childList: true, subtree: false }
+  const observer = new MutationObserver(mutationCallback)
+  observer.observe(portalApplicationList, config)
+}
+
+window.onload = async () => {
+  window.setTimeout(
+    () => observePortalApplicationList(onAwsAccountSelected),
+    1 * 1000
+  )
+}
