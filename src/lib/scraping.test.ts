0420fa496a7fa5bc40aa185cbf0ddb3fdcffe8ee
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-06-27 15:32:10 +0900

    chore: shorten test html

--- src/lib/scraping.test.ts
+++ src/lib/scraping.test.ts
@@ -5,7 +5,7 @@ test('toAccountNameAndId works', () => {
   const element = document.createElement('div');
   element.innerHTML = `
 <button class="" data-testid="account-list-cell" aria-expanded="false">
-  <svg><path></path></svg>
+  <svg></svg>
   <span>
     <svg>
       <title>AWS Default Icon</title>
@@ -34,19 +34,10 @@ test('patchAccountNameIfAwsSso works: AWS SSO, Account Name', () => {
   document.body.innerHTML = `
 <button aria-controls="menu--account" aria-label="AWSReadOnlyAccess/hiroga" aria-expanded="false" data-testid="more-menu__awsc-nav-account-menu-button" id="nav-usernameMenu">
   <span data-testid="awsc-nav-account-menu-button">
-      <span title="AWSReservedSSO_AWSReadOnlyAccess_aaaaaaaa00000000/hiroga @ 1234-5678-9012">
-          AWSReadOnlyAccess/hiroga</span>
-      <span>
-          <svg>
-              <path></path>
-          </svg>
-      </span>
+      <span title="AWSReservedSSO_AWSReadOnlyAccess_aaaaaaaa00000000/hiroga @ 1234-5678-9012">AWSReadOnlyAccess/hiroga</span>
+      <span></span>
   </span>
-  <span>
-      <svg>
-          <path></path>
-      </svg>
-  </span>
+  <span></span>
 </button>`
   patchAccountNameIfAwsSso({ accountId: '123456789012', accountName: 'Dev' })
   const accountMenuButtonSpan = getAccountMenuButtonSpan()
